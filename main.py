eca6ef3f1f9be357a1e0b995ff1b8553518cbe90
stevencarrau <stevencarr@utexas.edu>
2022-04-20 14:48:40 -0500

    Cfg loader

--- main.py
+++ main.py
@@ -1,40 +1,12 @@
+import json
 from experiment import Experiment
-import gridstorm
 
-cfg = {'name':'maze2',
-'formula_str':'Rmin=? [F "goal"]',
-    'batch_size':128,
-       'epochs':32, #
-       'ctrx_gen':'crt_full', # Counterexample generation
-       'rounds':10,
-       'p_init':{'sl':0.2}, # First parameter initialization
-       'p_evals': [{'sl': 0.1}, {'sl': 0.2}, {'sl': 0.3}, {'sl': 0.4}],
-       'p_bounds':{'sl':[0.1,0.4]},
-       'a_memory_dim':3,
-       'bottleneck_dim':2, # Base for number of memory nodes
-       'a_lr':0.001,
-       'r_lr': 0.001,
-       'length':10,
-       'r_batch_size':128,
-       'r_epochs':16,
-       'a_batch_size':128,
-       'a_epochs':16,
-       'num_hx_qbns':8,
-       'method': 'QBN',
-       'memory_dim':4,
-       'pso_n':500,
-       'pso_fr':0.5,
-       'blow_up':2,
-       'policy': 'qmdp', # mdp or qmdp
-'pso_lr':0.1,
-'pso_ar':0.2,
-'pso_rounds':5,
-'pso_phi_g':0.5,
-'pso_phi_r':0.5,
-'batch_dim':4
-}
+filename = 'maze2'
+with open('data/input/cfgs/'+filename+'.json') as f:
+   load_file = json.load(f)
+   cfg = load_file[filename][0]
 
-exp = Experiment('test',cfg,10)
+exp = Experiment(filename,cfg,10)
 
 exp.execute(False)
 
