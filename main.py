94caeccb903e5a85b9be82a4c1a48cc6d57525a1
tdsimao <tdsimao@users.noreply.github.com>
2022-04-20 16:20:26 -0500

    run experiments with mdp and qmdp policies

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ with open('data/input/cfgs/'+filename+'.json') as f:
    load_file = json.load(f)
    cfg = load_file[filename][0]
 
-exp = Experiment(filename,cfg,10)
-
-exp.execute(False)
-
-
+for policy in ["qmdp", "mdp"]:
+   cfg["policy"] = policy
+   exp = Experiment(filename + "_" + policy, cfg, 10)
+   exp.execute(False)
